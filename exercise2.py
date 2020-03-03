4c532e3b73b09f3638c4d866a099e050e5e904bd
Cyvelle Torefiel <cyvtorefiel23@gmail.com>
2020-03-03 19:55:30 +0800

    Adding exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+#Convert Celsius to Fahrenheit
+
+#Asks for user-input
+a = input("Enter temperature in Celsius: ")
+
+#Converts a to a float
+a = float(a)
+
+#C to F formula
+b = (a*(9/5))+32
+
+print ("Temperature in Fahrenheit: ", b)
